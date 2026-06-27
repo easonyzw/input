3c9a8aef66ce193bec7c469053542689ab386a55
easonyzw <2418493731@qq.com>
2026-06-27 23:07:30 +0800

    Add gitignore and remove build artifacts

--- Build/startup_stm32f051.d
+++
@@ -1,1 +0,0 @@
-.\build\startup_stm32f051.o: Libraries\CMSIS\DeviceSupport\startup\startup_stm32f051.s
--- Build/stm32f0xx_it.d
+++
@@ -1,2 +0,0 @@
-.\build\stm32f0xx_it.o: main\stm32f0xx_it.c
-.\build\stm32f0xx_it.o: main\stm32f0xx_it.h
