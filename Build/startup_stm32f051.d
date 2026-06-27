22af3321988b3be730203e5fb359c71aeca82462
easonyzw <2418493731@qq.com>
2026-06-27 23:00:42 +0800

    first commit

---
+++ Build/startup_stm32f051.d
@@ -0,0 +1,1 @@
+.\build\startup_stm32f051.o: Libraries\CMSIS\DeviceSupport\startup\startup_stm32f051.s
---
+++ Build/stm32f0xx_it.d
@@ -0,0 +1,2 @@
+.\build\stm32f0xx_it.o: main\stm32f0xx_it.c
+.\build\stm32f0xx_it.o: main\stm32f0xx_it.h
