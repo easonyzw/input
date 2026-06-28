d264d58e0fd44b82b651ab3922a07143b5918fdd
easonyzw <101691537+easonyzw@users.noreply.github.com>
2026-06-28 22:24:03 +0800

    Restore initial empty main loop

--- main/main.c
+++ main/main.c
@@ -1,12 +1,7 @@
-#include "Hardware.h"
-
+#include "stm32f0xx.h"
 int main(void)
 {
-    hardware_init();
-    software_init();
-
-    while(1)
+    while (1)
     {
-        IO_state_update();
     }
 }
