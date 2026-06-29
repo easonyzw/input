531844645f926f1316721ba36add6c18f5e04623
easonyzw <2418493731@qq.com>
2026-06-29 23:18:16 +0800

    1

--- Hardware/Rcc.c
+++ Hardware/Rcc.c
@@ -1,9 +1,31 @@
-
+#include "Rcc.h"
 void rcc_init(void)
 {
-	
-	
-	
-	void RCC_HSEConfig(RCC_HSE_ON);
-	
+    ErrorStatus status;
+
+    RCC_ClockSecuritySystemCmd(ENABLE);
+
+    RCC_HSEConfig(RCC_HSE_ON);
+    status = RCC_WaitForHSEStartUp();
+
+    if (status == SUCCESS)
+    {
+        FLASH_PrefetchBufferCmd(ENABLE);
+
+        RCC_HCLKConfig(RCC_SYSCLK_Div1);
+        RCC_PCLKConfig(RCC_HCLK_Div1);
+
+        RCC_PLLConfig(RCC_PLLSource_HSE, RCC_PLLMul_6);
+        RCC_PLLCmd(ENABLE);
+
+        while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+        {
+        }
+
+        RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+
+        while (RCC_GetSYSCLKSource() != 0x08)
+        {
+        }
+    }
 }
---
+++ Hardware/Rcc.h
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef _RCC_H
+#define _RCC_H
+
+
+
+void Rcc_Init(void);
+
+
+#endif
